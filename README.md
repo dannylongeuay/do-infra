108d3f29b2a098fbe0195853a713ab3f7ad68112
Daniel Longeuay <93006157+dannylongeuay@users.noreply.github.com>
2021-11-19 15:18:57 -0800

    refactor: pulumi typescript ci/cd workflow (#1)
    * chore: doc file change
    
    * fix: :bug: updated ci workflow
    
    * fix: :bug: added pulumi access token env
    
    * fix: :bug: enter virtual environment
    
    * fix: :bug: remove virtual env
    
    * chore: python debugging
    
    * chore: pin pulumi actions version
    
    * chore: export requirements
    
    * chore: freestyle pulumi
    
    * fix: poetry typo
    
    * fix: fix env var case
    
    * chore: select stack
    
    * chore: outputs test
    
    * fix: install dev dependencies
    
    * fix: set output
    
    * chore: multiline preview
    
    * fix: remove debug echo
    
    * chore: :wastebasket: workflow clean up
    
    * chore: hide and recreate
    
    * refactor: :recycle: typescript workflow
    
    * fix: :bug: indentation
    
    * chore: :wrench: remove pr comments
    
    * feat: :sparkles: added cd workflow

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# do-infra
+# Digital Ocean Infrastructure As Code
 
 Manage Digital Ocean Infrastructure
 
 ```
 export DIGITALOCEAN_TOKEN=XXXXXXXXXXXXXX
-```
-
-```
-make shell
 ```
 
 ```
--- __main__.py
+++
@@ -1,8 +0,0 @@
-"""A DigitalOcean Python Pulumi program"""
-
-import pulumi
-import pulumi_digitalocean as do
-
-domain = do.Domain('ndsquared', name='ndsquared.net')
-
-pulumi.export('domain_name', domain.name)
