066d3309c0a8a0caf02e40ead0302b5082cc84c0
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-11-19 16:52:19 -0800

    feat: :sparkles: added k8s cluster

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,10 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
-// Create a DigitalOcean resource (Domain)
 const domain = new digitalocean.Domain("ndsq-domain", {
   name: "ndsquared.net",
 });
 
-// Export the name of the domain
-export const domainName = domain.name;
+const cluster = new digitalocean.KubernetesCluster("ndsq-cluster", {
+  name: "ndsquared-prod-sf03",
+  autoUpgrade: true,
+  region: digitalocean.Region.SFO3,
+  version: digitalocean.getKubernetesVersions().then((p) => p.latestVersion),
+  nodePool: {
+    name: "default",
+    size: digitalocean.DropletSlug.DropletS1VCPU1GB,
+    nodeCount: 3,
+  },
+  tags: ["ndsquared"],
+});
